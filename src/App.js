3b036adbfde0da125c036b98451643a1a576f091
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-10 17:01:43 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React from 'react'; 
 import './App.css'; 
-import Book from './book'
+import Book from './book';
+
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
+  Title: 'El Manifiesto Comunista', 
+  author: 'Karl Marx y Friedrich Engels'
+};
+
+const secondBook = {
+  img: 'https://books.google.pt/books/publisher/content?id=nwJgEAAAQBAJ&hl=es&pg=PA1&img=1&zoom=3&bul=1&sig=ACfU3U26KbejCbCJv-4pszlMW3Cjn4AUAg&w=1280',
+  Title: 'El Estado y la Revolución', 
+  author: 'Vladimir Lenin'
+};
+
+
 
 
 
 function App() {
   return (
-  <section className='booklist'>
-    <Book job='Developer'/>
-    
-  </section>
+    <section className='booklist'>
+      <Book img={firstBook.img} Title={firstBook.Title} author={firstBook.author} />
+      <Book img={secondBook.img} Title={secondBook.Title} author={secondBook.author} />
+    </section>
   );
 }
 
--- src/book.js
+++ src/book.js
@@ -1,35 +1,16 @@
 import React from 'react'; 
 import './App.css'; 
 
-
-
 // importante esto 
-const Tittle = 'El Manifiesto Comunista'; 
-const author = 'Karl Marx y Friedrich Engels'
-const img ='https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg';
 
- 
-
-//Props
-
-function Book(props){
-return(
-
-<article className='book'>
-   <img src={img} alt='' width='50px' />
-<h1>{Tittle}</h1>
-<h4>{author}</h4>
-<p>{props.job}</p>
-</article>
-
-
-
-
-)
-
-
-
+function Book(props) {
+  return (
+    <article className='book'>
+      <img src={props.img} alt='' width='50px' />
+      <h1>{props.Title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
 }
 
-
-export default Book
+export default Book;
