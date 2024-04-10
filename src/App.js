5b7c4ed3b6545eb3bcbb84f303b9c99ba0f4262b
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-10 18:31:10 +0100

    add

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import React from 'react';
 import './App.css'; 
 import Book from './book';
 
-const firstBook = {
+const books= [
+{
+  id: '1',
   img: 'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
   Title: 'El Manifiesto Comunista', 
   author: 'Karl Marx y Friedrich Engels'
-};
+},
 
-const secondBook = {
+ {
+  id: '2',
   img: 'https://books.google.pt/books/publisher/content?id=nwJgEAAAQBAJ&hl=es&pg=PA1&img=1&zoom=3&bul=1&sig=ACfU3U26KbejCbCJv-4pszlMW3Cjn4AUAg&w=1280',
   Title: 'El Estado y la Revolución', 
   author: 'Vladimir Lenin'
-};
+},
+
+{
+  id: '3',
+  img: 'https://m.media-amazon.com/images/I/71qHGvQQJSL._AC_UF1000,1000_QL80_.jpg',
+  Title: 'El Capital', 
+  author: 'Karl Marx'
+},
 
 
+];
 
 
 
 function App() {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} Title={firstBook.Title} author={firstBook.author} />
-      <Book img={secondBook.img} Title={secondBook.Title} author={secondBook.author} />
+       {books.map((book, index) => (
+        <Book 
+          key={book.id}
+          img={book.img}
+          Title={book.Title} 
+          author={book.author} 
+        />
+      ))}
     </section>
   );
 }
--- src/book.js
+++ src/book.js
@@ -1,14 +1,15 @@
-import React from 'react'; 
+import React from 'react';
 import './App.css'; 
 
 // importante esto 
 
-function Book(props) {
+function Book(props) { 
   return (
     <article className='book'>
       <img src={props.img} alt='' width='50px' />
       <h1>{props.Title}</h1>
       <h4>{props.author}</h4>
+      {props.children}
     </article>
   );
 }
