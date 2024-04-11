f6cd53387cacac70a73caebc24704a0b52f6518a
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-11 18:55:32 +0100

    final almost

--- src/book.js
+++ src/book.js
@@ -14,10 +14,11 @@ function Book(props) {
     <article className='book'>
       <img src={props.img} alt='' width='50px' />
       <h1>{props.Title}</h1>
-      <h4>{props.author}</h4>
-      <button type='button' onClick={clickHandler}> example</button>
+      <h4 style={{color:'#617d98' , fontSize: '0.75rem', 
+    marginTop: '0.25rem'}}>{props.author}</h4>
     </article>
   );
 }
 
 export default Book;
+
--- src/books.js
+++ src/books.js
@@ -22,10 +22,52 @@ export const books= [
 
     {
         id: '4',
-        img: 'https://m.media-amazon.com/images/I/712yKeC3kHL._SY466_.jpg',
-        Title: 'El socialismo traicionado', 
-        author: 'Roger Keeran y Thomas Kenny'
+        img: 'https://m.media-amazon.com/images/I/71hSCZR9ygL._AC_UY327_FMwebp_QL65_.jpg',
+        Title: 'Imperialismo, fase superior del capitalismo', 
+        author: 'Vladimir Lenin'
       },
+    
+      {
+        id: '5',
+        img: 'https://m.media-amazon.com/images/I/419V+kPb+QL._SY445_SX342_.jpg',
+        Title: '¿Qué hacer?', 
+        author: 'Vladimir Lenin'
+      },
+      {
+        id: '6',
+        img: 'https://m.media-amazon.com/images/I/81BbXiJvHEL._AC_UY327_FMwebp_QL65_.jpg',
+        Title: 'La Revolución Rusa', 
+        author: 'Sheila Fitzpatrick'
+      },
+    
+      {
+        id: '7',
+        img: 'https://m.media-amazon.com/images/I/61r5LLAg0XL._AC_UY327_FMwebp_QL65_.jpg',
+        Title: 'El comunismo: una breve introducción', 
+        author: 'David Priestland'
+      },
+    
+      {
+        id: '8',
+        img: 'https://m.media-amazon.com/images/I/51pHKuRPu1L._AC_UY327_FMwebp_QL65_.jpg',
+        Title: 'La acumulación del capital', 
+        author: 'Rosa Luxemburgo'
+      },
+    
+      {
+        id: '9',
+        img: 'https://m.media-amazon.com/images/I/71amU3fI+3L._AC_UY327_FMwebp_QL65_.jpg',
+        Title: 'La teoría de la alienación en Marx', 
+        author: 'István Mészáros'
+      },
+    
+      {
+        id: '10',
+        img: 'https://m.media-amazon.com/images/I/71Jb8O+Av-L._AC_UY327_FMwebp_QL65_.jpg',
+        Title: 'El marxismo y la cuestión nacional', 
+        author: 'Joseph Stalin'
+      },
+    
     
     
     ];
